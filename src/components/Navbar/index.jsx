87aa6f05be07d543d011acf5068e1fdcfbc4f1bb
Madina <madina.m.9509@gmail.com>
2024-05-09 10:58:13 +0500

    installed i18next framework for translation & styled-components

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,24 +1,37 @@
 import { Link } from 'react-router-dom';
 import { ROUTES_ARRAY } from '../../routes';
+import styled from 'styled-components';
 
 const Navbar = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'space-around',
-        backgroundColor: 'yellow',
-      }}
-    >
-      <span>Logo</span>
-      {ROUTES_ARRAY.map((navItem) => (
-        <Link key={navItem.path} to={navItem.path}>
-          {navItem.title}
-        </Link>
-      ))}
-      <span>smth</span>
-    </div>
+    <>
+      <Container>
+        <div className='nav'>
+          <span>Logo</span>
+          {ROUTES_ARRAY.map((navItem) => (
+            <Link key={navItem.path} to={navItem.path}>
+              {navItem.title}
+            </Link>
+          ))}
+          <span>smth</span>
+        </div>
+      </Container>
+    </>
   );
 };
+
+const Container = styled.div`
+  max-width: 1320px;
+  margin: 0 auto;
+
+  a {
+    text-decoration: none;
+    color: #000;
+  }
+  .nav {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
 
 export default Navbar;
--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,17 +1,36 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../../components/Navbar/index';
-import './layout.css';
+import styled from 'styled-components';
 
 export const Layout = () => {
   return (
-    <div className='layout'>
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <Outlet />
-      </main>
-      <footer>Footer</footer>
-    </div>
+    <>
+      <Container>
+        <div className='layout'>
+          <header>
+            <Navbar />
+          </header>
+          <main>
+            <Outlet />
+          </main>
+          <footer>Footer</footer>
+        </div>
+      </Container>
+    </>
   );
 };
+
+const Container = styled.div`
+  width: 1320px;
+  margin: 0 auto;
+
+  .layout {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+
+  .layout main {
+    flex-grow: 1;
+  }
+`;
